1e1562c0acd4b85943edfb740f421e7a11481f6d
root <root@m0th3r>
2019-10-03 11:14:07 -0400

    just a quick edit on 'others.h'

--- others.h
+++ others.h
@@ -1,11 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-/* can't decide if the following should be part of manifesto.c or not...
-#define COMPUTER_SCIENCE 1
-#define ART 1
-#define THERE_EXISTS void
-#define A_QUESTION main
-*/
 #define ATTEMPT_AN_ANSWER {\
     FILE *fptr;\
     char* filename = ".manifesto.txt\0";\
